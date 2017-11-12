1d697936758b38acee505635fb651492435ab150
mathgomes <mtgomes6644@gmail.com>
2017-11-12 17:15:01 -0200

    Final commit pt2

--- Server/routes/payments.js
+++ Server/routes/payments.js
@@ -7,7 +7,7 @@ router.post("/", middleware.checkConstraints, function(req, res) {
     paymentModel.create(req.body, function(err, createdPayment) {
         if(err || !createdPayment) {
             var message = "One or more fields are not defined or incorrect. " +
-            "Please, insert all the fields correctly";
+            "Please, insert all the fields correctly. (" + err.message + ")";
             return res.status(400).json({message: message});
         }
         else res.status(200).json(createdPayment);
